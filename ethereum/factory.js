de75b10293e3b7d95082aa2211d18d12826ca76f
Navneet <bosshero680@gmail.com>
2024-02-29 02:18:20 +0530

    updated syntex for web3 v4

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,9 @@
 import web3 from "./web3";
-import CampaignFactory from './build/CampaignFactory.json'
-
+import CampaignFactory from "./build/CampaignFactory.json";
+ 
 const instance = new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    '0x7f87F4f26E6D3aED7a59cEA6CA871F4444867203'
+  CampaignFactory.abi,
+  "PUT ADDRESS HERE AFTER DEPLOYING CONTRACT"
 );
-
+ 
 export default instance;
