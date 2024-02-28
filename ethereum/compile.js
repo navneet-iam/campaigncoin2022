7e4ee9e675e2ff71d56a5b49b61cc64b8ceb91c2
Navneet <bosshero680@gmail.com>
2024-02-29 02:12:52 +0530

    increased gas limit in testing, updated compilejs

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,19 +1,38 @@
-const path = require('path');
-const solc = require('solc');
-const fs = require('fs-extra');
-
-const buildPath = path.resolve(__dirname, 'build');
-fs.removeSync(buildPath);       //-->delete entire build folder
-
-const campaignPath = path.resolve(__dirname, 'contracts', 'campaign.sol');
-const source = fs.readFileSync(campaignPath, 'utf8');       //-->read compile.sol from contract folder
-const output = solc.compile(source, 1).contracts;
-
-fs.ensureDirSync(buildPath);        // check if directory already exist, if not creates one
-// console.log(output);
-for(let contract in output) {
-    fs.outputJSONSync(
-        path.resolve(buildPath, contract.replace(':', '') +'.json'),
-        output[contract]
-    );
+const path = require("path");
+const solc = require("solc");
+const fs = require("fs-extra");
+ 
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath);
+ 
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
+ 
+const input = {
+  language: "Solidity",
+  sources: {
+    "Campaign.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+ 
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  "Campaign.sol"
+];
+ 
+fs.ensureDirSync(buildPath);
+ 
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(":", "") + ".json"),
+    output[contract]
+  );
 }
